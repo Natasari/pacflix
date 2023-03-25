1a95d869cbc0ec3067a67ff025af265866ba24cb
Natasari <penitasari93@gmail.com>
2023-03-25 15:28:59 +0700

    menambahkan if

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Halo ini didalam document main.py")
 
 user_do = input("Anda ingin melakukan registrasi: Y/N ")
+if user_do == 'Y':
+    print("User akan registrasi")
+else:
+    print("User akan subscribe")
